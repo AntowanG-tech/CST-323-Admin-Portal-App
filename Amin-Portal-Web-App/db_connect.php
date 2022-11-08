94e6914d4e2010daf1b8933e636dd566ebb15d6e
AntowanG-tech <antowang93@gmail.com>
2022-11-07 23:39:04 -0800

    changed db_connect.php to the azure phpMyAdmin credentials.

--- Amin-Portal-Web-App/db_connect.php
+++ Amin-Portal-Web-App/db_connect.php
@@ -4,14 +4,14 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "localhost";
-$port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "lunaristechadmin";
+$host = "127.0.0.1:49519";
+//$port = "3306";
+$username = "azure";
+$user_pass = "6#vWHD_$";
+$database_in_use = "localdb";
 
 
-$mysqli = new mysqli($host, $port, $username, $user_pass, $database_in_use);
+$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
 if ($mysqli->connect_error) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
