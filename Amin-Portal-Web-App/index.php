aeeec5935091b79ef8f66a6cd764496366a1c358
AntowanG-tech <antowang93@gmail.com>
2022-11-18 20:37:32 -0800

    monolog index.php fix attempt #7 commented out line: 5

--- Amin-Portal-Web-App/index.php
+++ Amin-Portal-Web-App/index.php
@@ -2,7 +2,7 @@
 
 date_default_timezone_set('America/Phoenix');
 
-require _DIR_ . '/vendor/autoload.php';
+//require _DIR_ . '/vendor/autoload.php';
 use Monolog\Level1;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
